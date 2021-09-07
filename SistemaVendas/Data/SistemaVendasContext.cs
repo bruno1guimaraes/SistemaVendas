615b5144819d2459a9a0fc103796fc8bab9bdc9f
bruno1guimaraes <bulima@gmail.com>
2021-09-07 10:55:07 -0300

    CRUD Departments

---
+++ SistemaVendas/Data/SistemaVendasContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SistemaVendas.Models
+{
+    public class SistemaVendasContext : DbContext
+    {
+        public SistemaVendasContext (DbContextOptions<SistemaVendasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemaVendas.Models.Department> Department { get; set; }
+    }
+}
