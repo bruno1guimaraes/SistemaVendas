4ff189dc42bf0f95eb23e688e86ef4171232f8dd
bruno1guimaraes <bulima@gmail.com>
2021-09-08 19:51:41 -0300

    Corrige Add Vendedor

--- SistemaVendas/Controllers/SellersController.cs
+++ SistemaVendas/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SistemaVendas.Models;
+using SistemaVendas.Models.ViewModels;
 using SistemaVendas.Services;
 
 namespace SistemaVendas.Controllers
@@ -7,9 +8,11 @@ namespace SistemaVendas.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
-        public SellersController(SellerService sellerService)
+        private readonly DepartmentService _departmentService;
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
         public IActionResult Index()
         {
@@ -18,7 +21,13 @@ namespace SistemaVendas.Controllers
         }
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel
+            {
+                Departments = departments
+
+            };
+            return View(viewModel);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
---
+++ SistemaVendas/Services/DepartmentService.cs
@@ -0,0 +1,20 @@
+﻿using SistemaVendas.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SistemaVendas.Services
+{
+    public class DepartmentService
+    {
+        private readonly SistemaVendasContext _context;
+        public DepartmentService(SistemaVendasContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
