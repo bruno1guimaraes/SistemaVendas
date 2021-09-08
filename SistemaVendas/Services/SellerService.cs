5cee794c679cdbeb0bfdb5b8d92e28a8749f1dab
bruno1guimaraes <bulima@gmail.com>
2021-09-08 09:58:35 -0300

    View Index de Vendedores

---
+++ SistemaVendas/Services/SellerService.cs
@@ -0,0 +1,21 @@
+﻿using SistemaVendas.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SistemaVendas.Services
+{
+    public class SellerService
+    {
+        private readonly SistemaVendasContext _context;
+        public SellerService(SistemaVendasContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+
+}
--- SistemaVendas/Startup.cs
+++ SistemaVendas/Startup.cs
@@ -12,6 +12,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SistemaVendas.Models;
 using SistemaVendas.Data;
+using SistemaVendas.Services;
 
 namespace SistemaVendas
 {
@@ -42,6 +43,7 @@ namespace SistemaVendas
                     builder.MigrationsAssembly("SistemaVendas")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
