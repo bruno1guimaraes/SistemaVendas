e6ad78e1bcc8b4a176af4ebab0a481236b49ca46
bruno1guimaraes <bulima@gmail.com>
2021-09-08 21:47:39 -0300

    eager loading

--- SistemaVendas/Services/SellerService.cs
+++ SistemaVendas/Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using SistemaVendas.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace SistemaVendas.Services
 {
@@ -23,7 +24,7 @@ namespace SistemaVendas.Services
         }
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         public void Remove(int id)
         {
