2c99e6d5a2ff54c00b48269ac60c2b51f3de85c2
bruno1guimaraes <bulima@gmail.com>
2021-09-09 15:27:49 -0300

    Customizações

--- SistemaVendas/Models/SalesRecord.cs
+++ SistemaVendas/Models/SalesRecord.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using SistemaVendas.Models.Enums;
 
 namespace SistemaVendas.Models
@@ -6,9 +7,13 @@ namespace SistemaVendas.Models
     public class SalesRecord
     {
         public int Id { get; set; }
+        [Display(Name = "Data da venda")]
         public DateTime Date { get; set; }
+        [Display(Name = "Valor Total")]
         public double Amount { get; set; }
+        [Display(Name = "Situação do pedido")]
         public SaleStatus Status { get; set; }
+        [Display(Name = "Vendedor")]
         public Seller Seller { get; set; }
 
         public SalesRecord()
